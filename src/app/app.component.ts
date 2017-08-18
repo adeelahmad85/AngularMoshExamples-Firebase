8001f1f3d71fbf9a2b2508d45654306d063d0237
Adeel Ahmed <adeelahmad@msn.com>
2017-08-18 19:33:57 +0500

    175. Unsubscribing from Subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from "rxjs/Subscription";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy {
+  courses: any[];
+  subscription: Subscription
+
+  constructor(db: AngularFireDatabase){
+    this.subscription = db.list('/courses').subscribe(courses => {
+      this.courses = courses;
+      console.log(this.courses);
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAMcU78RwmV3rbVtqRBN7z1HTp0seMYjYw",
+    authDomain: "fir-demo-497e0.firebaseapp.com",
+    databaseURL: "https://fir-demo-497e0.firebaseio.com",
+    projectId: "fir-demo-497e0",
+    storageBucket: "fir-demo-497e0.appspot.com",
+    messagingSenderId: "959099100198"
+  }
 };
